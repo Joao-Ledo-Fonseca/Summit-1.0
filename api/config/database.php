b72fceb41080e505eb2380e62226d15e86cda59d
Joao-Ledo-Fonseca <joao.ledo.fonseca@gmail.com>
2020-07-14 13:05:23 +0100

    Create database.php

---
+++ api/config/database.php
@@ -0,0 +1,29 @@
+<?php
+class Database{
+ 
+    // specify your own database credentials
+    public $host = "localhost";
+    // public $db_name = "id11905308_test";
+    // public $username = "id11905308_test";
+    // public $password = "testiefp";
+    public $db_name = "summit";
+    public $username = "summit";
+    public $password = "summit";
+    public $conn;
+ 
+    // get the database connection
+    public function getConnection(){
+ 
+        $this->conn = null;
+ 
+        try{
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        }catch(PDOException $exception){
+            echo "Connection error: " . $exception->getMessage();
+        }
+ 
+        return $this->conn;
+    }
+}
+?>
