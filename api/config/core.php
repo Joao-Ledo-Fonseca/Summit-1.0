5554c7a1a79230befa5a8dfa9d68da206c1b1046
Joao-Ledo-Fonseca <joao.ledo.fonseca@gmail.com>
2020-07-14 13:00:46 +0100

    Create core.php

---
+++ api/config/core.php
@@ -0,0 +1,22 @@
+<?php
+// show error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+ 
+// test mode on/off
+$test=FALSE ;
+$test_email="joao-ledo.fonseca@gmail.com";
+
+// home page url
+$home_url="http://localhost:81/api/";
+$external_url="http://summit.localhost:81/api/";
+ 
+// page given in URL parameter, default page is one
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+ 
+// set number of records per page
+$records_per_page = 5;
+ 
+// calculate for the query LIMIT clause
+$from_record_num = ($records_per_page * $page) - $records_per_page;
+?>
